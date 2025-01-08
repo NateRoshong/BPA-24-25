ee9e7ff8b865fe5fae59b6d599e98e8c1d6bc15e
811237 <142992805+811237@users.noreply.github.com>
2025-01-08 10:34:36 -0500

    cont on last commit, trying to figure out on how to edit "--bs-dropdown-min-width: 10rem;" to change the dropdown menu

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+//JQUERIE SCRIPT START
+$(document).ready(function() {
+    function adjustNavBrandsHolder() {
+        if ($(window).width() <= 599) {
+            $('#nav-brands-holder').addClass('dropdown-menu');
+        } else {
+            $('#nav-brands-holder').removeClass('dropdown-menu');
+        }
+    }
+    adjustNavBrandsHolder();
+    //Adjusts nav-brands-holder on window resize
+    $(window).resize(function() {
+        adjustNavBrandsHolder();
+    });
+});
